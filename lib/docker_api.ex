2b1d79c4379f17298a4e194bfa693721841b4cc8
Riki Ueno <3860711-g_kenkun@users.noreply.gitlab.com>
2020-10-25 18:50:30 +0900

    Remove DockerAPI's default hello function

--- lib/docker_api.ex
+++ lib/docker_api.ex
@@ -1,18 +1,3 @@
 defmodule DockerAPI do
-  @moduledoc """
-  Documentation for `DockerAPI`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DockerAPI.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
